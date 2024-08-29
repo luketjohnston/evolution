cb8cb0d90805612269f0f0c200028084213f9af0
Luke Johnston <luketjohnston32@gmail.com>
2024-08-29 11:42:43 -0700

    still working on binary kernel, cleaned up a bit

--- kernels/binary/binary.cu
+++ kernels/binary/binary.cu
@@ -21,6 +21,32 @@
   ((byte) & 0x02 ? '1' : '0'), \
   ((byte) & 0x01 ? '1' : '0') 
 
+const bool VERBOSE{false};
+const bool COMPARE_WITH_CPU{true};
+const unsigned int BATCH_SIZE{128}; // TODO when we make 32 again, change actual kernel call threads also
+const unsigned int POPULATION_SIZE{16};
+// IN_SIZE must be large enough for a full warp to load integers into memory.
+// so it has to be at least 32*32
+const unsigned int IN_SIZE{32*32}; // in bits
+const unsigned int OUT_SIZE{128}; // in bits
+const unsigned int IN_INTS{IN_SIZE / 8 / sizeof(unsigned long)}; 
+const unsigned int OUT_INTS{OUT_SIZE / 8 / sizeof(unsigned long)};
+
+// 32 * 32 = 1024, the max threads per block.
+// a full warp (32 threads) is used to compute a single output
+// haven't tested anything besides 1
+const unsigned int OUT_TILE_X_INTS{1};
+// needs to stay at 32 to match warp size
+const unsigned int OUT_TILE_Y_INTS{32};
+
+// Needs to stay at 32 to match warp size
+const unsigned int SUBTILE_INTS{32};
+
+const unsigned int WEIGHT_SIZE{POPULATION_SIZE * IN_SIZE * OUT_SIZE * 2}; // *2 since we have a weight for the input and for the inverted input
+const unsigned int WEIGHT_INTS{WEIGHT_SIZE / 8 / sizeof(unsigned long)};
+
+namespace binary_forward {
+
 typedef unsigned long myIntType_t;
 
 __host__ __device__ void printbinary(myIntType_t i) {
@@ -35,14 +61,12 @@ __host__ __device__ void printbinary(unsigned char i) {
     printf(BBP "\n", BB(i));
 }
 
-const bool VERBOSE{false};
 
 typedef unsigned int intType_t;
 
 
 const intType_t WARP_SIZE{32};
 
-const bool COMPARE_WITH_CPU{true};
 
 /**
 In order to use all 32 warps at the final ballot_sync, we need the output
@@ -51,76 +75,55 @@ This means that each of the 32 warps must compute popcount(w * input)
 */
 
 
-const intType_t BATCH_SIZE{128}; // TODO when we make 32 again, change actual kernel call threads also
-const intType_t POPULATION_SIZE{16};
-
-const intType_t IN_SIZE{32*32}; // in bits
-const intType_t OUT_SIZE{128}; // in bits
-
-// IN_SIZE must be large enough for a full warp to load integers into memory.
-// so it has to be at least 32*32
-//const intType_t IN_SIZE{32*32}; // in bits
-//const intType_t OUT_SIZE{32*32}; // in bits
-
-// NOTE when allocating etc we still need to multiply this by BATCH_SIZE and POPULATION_SIZE when 
-// appropriate
-const intType_t IN_BYTES{IN_SIZE / 8};
-const intType_t OUT_BYTES{OUT_SIZE / 8};
-
-const intType_t IN_INTS{IN_BYTES / sizeof(myIntType_t)}; // TODO  can we use 64 bit ints?
-const intType_t OUT_INTS{OUT_BYTES / sizeof(myIntType_t)};
-
-// 32 * 32 = 1024, the max threads per block.
-// a full warp (32 threads) is used to compute a single output
-// haven't tested anything besides 1
-const intType_t OUT_TILE_X_INTS{1};
-// needs to stay at 32 to match warp size
-const intType_t OUT_TILE_Y_INTS{32};
-
-// Needs to stay at 32 to match warp size
-const intType_t SUBTILE_INTS{32};
-
-const intType_t WEIGHT_SIZE{POPULATION_SIZE * IN_SIZE * OUT_SIZE * 2}; // *2 since we have a weight for the input and for the inverted input
-const intType_t WEIGHT_BYTES{WEIGHT_SIZE / 8};
-const intType_t WEIGHT_INTS{WEIGHT_BYTES / sizeof(myIntType_t)};
 
 
 // note that i is the index into the input as an array of integers, NOT as an array of bits.
-__host__ __device__ intType_t i_ind(const intType_t b, const intType_t p, const intType_t i) {
+__host__ __device__ intType_t i_ind(const intType_t b, const intType_t p, const intType_t i, const myIntType_t population_size, const myIntType_t in_ints) {
  
-  return b * POPULATION_SIZE * IN_INTS + p * IN_INTS + i;
+  return b * population_size * in_ints + p * in_ints + i;
 }
 
 // note that o is the index into output as an array of integers, NOT as an array of bits.
-__host__ __device__ intType_t o_ind(const intType_t b, const intType_t p, const intType_t o) {
-  return b * POPULATION_SIZE * OUT_INTS + p * OUT_INTS + o;
+__host__ __device__ intType_t o_ind(const intType_t b, const intType_t p, const intType_t o, const myIntType_t population_size, const myIntType_t out_ints) {
+  return b * population_size * out_ints + p * out_ints + o;
 }
 
 // note the "o" arg to w_ind should be different than the "o" arg to o_ind above.
 // (because w has a full integer entry for each output bit o)
-__host__ __device__ intType_t w_ind(const intType_t t, const intType_t p, const intType_t i, const intType_t o) {
-  return t * IN_INTS * POPULATION_SIZE * OUT_SIZE + p * IN_INTS * OUT_SIZE + i * OUT_SIZE + o;
+__host__ __device__ intType_t w_ind(const intType_t t, const intType_t p, const intType_t i, const intType_t o, const myIntType_t population_size, const myIntType_t in_ints, const myIntType_t out_size) {
+  return t * in_ints * population_size * out_size + p * in_ints * out_size + i * out_size + o;
 }
 
 
 
 // Computes the binary matrix multiplication on the host and saves the output
 // out should be zeroed
-void host_op(myIntType_t const * const input, myIntType_t const * const weight, myIntType_t * out, int thresh) {
-  for( intType_t b = 0; b < BATCH_SIZE; b++) {
-    for( intType_t p = 0; p < POPULATION_SIZE; p++) {
-      for( intType_t o = 0; o < OUT_SIZE; o++) {
+void host_op(
+    myIntType_t const * const input, 
+    myIntType_t const * const weight, 
+    myIntType_t * out, 
+    int thresh,
+    const myIntType_t population_size,
+    const myIntType_t batch_size,
+    const myIntType_t in_ints,
+    const myIntType_t out_size
+    ) {
+  const myIntType_t out_ints = out_size / 8 / sizeof(unsigned long);
+
+  for( intType_t b = 0; b < batch_size; b++) {
+    for( intType_t p = 0; p < population_size; p++) {
+      for( intType_t o = 0; o < out_size; o++) {
 
         unsigned int temp = 0;
 
-        for( intType_t i = 0; i < IN_INTS; i++) {
-          temp += __builtin_popcount( (input[i_ind(b,p,i)] & weight[w_ind(0,p,i,o)]));
-          temp += __builtin_popcount((~input[i_ind(b,p,i)]) & weight[w_ind(1,p,i,o)]);
+        for( intType_t i = 0; i < in_ints; i++) {
+          temp += __builtin_popcount( (input[i_ind(b,p,i,population_size,in_ints)] & weight[w_ind(0,p,i,o,population_size,in_ints,out_size)]));
+          temp += __builtin_popcount((~input[i_ind(b,p,i,population_size,in_ints)]) & weight[w_ind(1,p,i,o,population_size,in_ints,out_size)]);
         }
         // NOTE that the way we shift here, the outputs are ordered low-precision bits first
         myIntType_t bit_to_set = (temp >= thresh) << (o % (8 * sizeof(myIntType_t)));
         myIntType_t o_int_index = o / (8 * sizeof(myIntType_t));
-        out[o_ind(b,p,o_int_index)] = out[o_ind(b,p,o_int_index)] | bit_to_set;
+        out[o_ind(b,p,o_int_index,population_size,out_ints)] = out[o_ind(b,p,o_int_index,population_size,out_ints)] | bit_to_set;
       }
     }
   }
@@ -130,7 +133,7 @@ at::Tensor host_helper(at::Tensor input, at::Tensor weight, int thresh) {
   const unsigned int population_size = input.sizes()[0];
   const unsigned int batch_size = input.sizes()[1];
   const unsigned int input_size = input.sizes()[2];
-  const unsigned int output_size = weight.sizes()[3];
+  const unsigned int out_size = weight.sizes()[3];
   TORCH_CHECK(input.sizes().size() == 3); // population, batch, input
   TORCH_CHECK(weight.sizes().size() == 4); // 2, population, input, output
   // TODO we shouldn't need this check, need to make kernel work for any batch size etc.
@@ -146,10 +149,11 @@ at::Tensor host_helper(at::Tensor input, at::Tensor weight, int thresh) {
   const unsigned long* weight_ptr = (unsigned long *) weight_contig.data_ptr();
   at::Tensor output;
   auto options = input_contig.options().dtype(torch::kInt64);
-  const unsigned int out_ints  = (output_size + 63) / 64;
+  const unsigned int out_ints  = (out_size + 63) / 64;
+  const unsigned int in_ints  = (input_size + 63) / 64;
   output = torch::empty({population_size*batch_size*out_ints}, options);
   unsigned long* output_ptr = (unsigned long *) output.data_ptr();
-  host_op(input_ptr, weight_ptr, output_ptr, thresh);
+  host_op(input_ptr, weight_ptr, output_ptr, thresh, population_size, batch_size, in_ints, out_size);
   return output;
 }
 
@@ -174,7 +178,10 @@ __global__ void binary_forward(
         const unsigned int out_tile_y_ints, 
         const unsigned int subtile_ints,
         const unsigned int in_ints, 
-        const unsigned int warp_size
+        const unsigned int warp_size,
+        const unsigned int population_size,
+        const unsigned int batch_size,
+        const unsigned int out_size
         ) {
 
   extern __shared__ myIntType_t shared[];
@@ -187,6 +194,7 @@ __global__ void binary_forward(
   const intType_t output_tile_x = blockIdx.x * out_tile_x_ints;
   const intType_t output_tile_y = blockIdx.y * out_tile_y_ints;
   const intType_t b = output_tile_y + threadIdx.y;
+  const unsigned int out_ints = out_size / 8 / sizeof(unsigned long);
 
   const intType_t p = blockIdx.z;
 
@@ -198,15 +206,15 @@ __global__ void binary_forward(
 
     // Load input into shared memory
     if (threadIdx.x < subtile_ints) { 
-      input_tile[threadIdx.y * subtile_ints + threadIdx.x] = input[i_ind(b, p, i)]; 
+      input_tile[threadIdx.y * subtile_ints + threadIdx.x] = input[i_ind(b, p, i, population_size, in_ints)]; 
     }
     // load weight into shared memory
     intType_t w_i_to_load =  tile_i * subtile_ints + threadIdx.y;
     intType_t w_o_to_load = blockIdx.x * out_tile_x_ints * 32 + threadIdx.x;
 
     // "o" varies with threadIdx.x and is the last coord so access is coalesced
-    weight_tile[    threadIdx.y * 32 +  threadIdx.x] = weight[w_ind(0, p, w_i_to_load, w_o_to_load)]; 
-    not_weight_tile[threadIdx.y * 32 +  threadIdx.x] = weight[w_ind(1, p, w_i_to_load, w_o_to_load)]; 
+    weight_tile[    threadIdx.y * 32 +  threadIdx.x] = weight[w_ind(0, p, w_i_to_load, w_o_to_load, population_size, in_ints, out_size)]; 
+    not_weight_tile[threadIdx.y * 32 +  threadIdx.x] = weight[w_ind(1, p, w_i_to_load, w_o_to_load, population_size, in_ints, out_size)]; 
 
     __syncthreads(); // wait until shared memory load is finished before continuing with compute
 
@@ -219,7 +227,7 @@ __global__ void binary_forward(
     __syncthreads(); // wait until this compute is finished before next loop replaces shared memory
 
   }
-  out[o_ind(b,p,output_tile_x + threadIdx.x)] = acc;
+  out[o_ind(b,p,output_tile_x + threadIdx.x,population_size,out_ints)] = acc;
 }
 
 __global__ void binary_forward_with_threshold(
@@ -231,7 +239,10 @@ __global__ void binary_forward_with_threshold(
         const unsigned int out_tile_y_ints, 
         const unsigned int subtile_ints,
         const unsigned int in_ints, 
-        const unsigned int warp_size
+        const unsigned int warp_size,
+        const unsigned int population_size,
+        const unsigned int batch_size,
+        const unsigned int out_size
         ) {
 
   extern __shared__ myIntType_t shared[];
@@ -244,6 +255,7 @@ __global__ void binary_forward_with_threshold(
   const intType_t output_tile_x = blockIdx.x * out_tile_x_ints;
   const intType_t output_tile_y = blockIdx.y * out_tile_y_ints;
   const intType_t b = output_tile_y + threadIdx.y;
+  const unsigned int out_ints = out_size / 8 / sizeof(unsigned long);
 
   const intType_t p = blockIdx.z;
 
@@ -255,15 +267,15 @@ __global__ void binary_forward_with_threshold(
 
     // Load input into shared memory
     if (threadIdx.x < subtile_ints) { 
-      input_tile[threadIdx.y * subtile_ints + threadIdx.x] = input[i_ind(b, p, i)]; 
+      input_tile[threadIdx.y * subtile_ints + threadIdx.x] = input[i_ind(b, p, i, population_size, in_ints)]; 
     }
     // load weight into shared memory
     intType_t w_i_to_load =  tile_i * subtile_ints + threadIdx.y;
     intType_t w_o_to_load = blockIdx.x * out_tile_x_ints * 32 + threadIdx.x;
 
     // "o" varies with threadIdx.x and is the last coord so access is coalesced
-    weight_tile[    threadIdx.y * 32 +  threadIdx.x] = weight[w_ind(0, p, w_i_to_load, w_o_to_load)]; 
-    not_weight_tile[threadIdx.y * 32 +  threadIdx.x] = weight[w_ind(1, p, w_i_to_load, w_o_to_load)]; 
+    weight_tile[    threadIdx.y * 32 +  threadIdx.x] = weight[w_ind(0, p, w_i_to_load, w_o_to_load, population_size, in_ints, out_size)]; 
+    not_weight_tile[threadIdx.y * 32 +  threadIdx.x] = weight[w_ind(1, p, w_i_to_load, w_o_to_load, population_size, in_ints, out_size)]; 
 
     __syncthreads(); // wait until shared memory load is finished before continuing with compute
 
@@ -281,7 +293,7 @@ __global__ void binary_forward_with_threshold(
   unsigned int r = __ballot_sync(__activemask(), acc >= thresh);
   // after ballot_sync, only one thread per warp needs to write to output
   if (threadIdx.x % warp_size == 0) {
-    out[o_ind(b,p,output_tile_x)] = r;
+    out[o_ind(b,p,output_tile_x,population_size,out_ints)] = r;
   }
 }
 
@@ -296,7 +308,7 @@ at::Tensor binary_forward_cuda(
   const unsigned int population_size = input.sizes()[0];
   const unsigned int batch_size = input.sizes()[1];
   const unsigned int input_size = input.sizes()[2];
-  const unsigned int output_size = weight.sizes()[3];
+  const unsigned int out_size = weight.sizes()[3];
   
   TORCH_CHECK(input.sizes().size() == 3); // population, batch, input
   TORCH_CHECK(weight.sizes().size() == 4); // 2, population, input, output
@@ -318,7 +330,7 @@ at::Tensor binary_forward_cuda(
 
 
   // TODO can we have result be shaped correctly or does it have to be contiguous?
-  //unsigned int output_tensor_size[1] = {population_size*batch_size*output_size};
+  //unsigned int output_tensor_size[1] = {population_size*batch_size*out_size};
   //unsigned int output_tensor_size_array_ref = ArrayRef(&output_tensor_size, 1);
 
   const unsigned long* input_ptr = (unsigned long *) input_contig.data_ptr();
@@ -336,9 +348,9 @@ at::Tensor binary_forward_cuda(
 
   if (thresh > 0) {
       // TODO change 64s to sizeof(unsigned long)
-      const unsigned int out_ints  = (output_size + 63) / 64;
+      const unsigned int out_ints  = (out_size + 63) / 64;
 
-      // TODO I don't think this works when output_size is not a multiple of 64
+      // TODO I don't think this works when out_size is not a multiple of 64
       auto options = input_contig.options().dtype(torch::kInt64);
 
       output = torch::empty({population_size*batch_size*out_ints}, options);
@@ -356,16 +368,19 @@ at::Tensor binary_forward_cuda(
               out_tile_y_ints,
               subtile_ints,
               in_ints,
-              warp_size
+              warp_size,
+              population_size,
+              batch_size,
+              out_size
               );
   } else {
       // here we have no thresholding, so the output tensor has to be larger
       auto options = input_contig.options().dtype(torch::kInt16); // 2**16 is plenty large enough hold activations
-      output = torch::zeros({population_size*batch_size*output_size}, options);
+      output = torch::zeros({population_size*batch_size*out_size}, options);
       unsigned short* output_ptr = (unsigned short *) output.data_ptr();
       const unsigned int out_tile_x_ints = 32;
      
-      const dim3 blocks(output_size, (batch_size + out_tile_y_ints - 1) / out_tile_y_ints, population_size);
+      const dim3 blocks(out_size, (batch_size + out_tile_y_ints - 1) / out_tile_y_ints, population_size);
 
       binary_forward<<<blocks, threads, sharedMemSize>>>(
               input_ptr, 
@@ -375,7 +390,10 @@ at::Tensor binary_forward_cuda(
               out_tile_y_ints,
               subtile_ints,
               in_ints,
-              warp_size
+              warp_size,
+              population_size,
+              batch_size,
+              out_size
               );
   }
       
@@ -383,9 +401,11 @@ at::Tensor binary_forward_cuda(
   return output;
 }
 
-//TORCH_LIBRARY_IMPL(extension_cpp, CUDA, m) {
+//TORCH_LIBRARY(binary_forward, m) {
 //  m.impl("binary_forward_with_threshold", &binary_forward_with_threshold);
 //}
+
+}
 
 
 
@@ -404,7 +424,7 @@ int main( int argc, char *argv[] )
   fprintf(stdout, "Batch size is %d\n",BATCH_SIZE);
   fprintf(stdout, "Population size is %d\n",POPULATION_SIZE);
   fprintf(stdout, "sizeof(int)%lu\n",sizeof(int));
-  fprintf(stdout, "sizeof(myIntType_t)%lu\n",sizeof(myIntType_t));
+  fprintf(stdout, "sizeof(myIntType_t)%lu\n",sizeof(binary_forward::myIntType_t));
 
   auto options =
       torch::TensorOptions()
@@ -438,7 +458,7 @@ int main( int argc, char *argv[] )
 
   checkCUDA( cudaEventRecord( start, 0 ) );
 
-  at::Tensor d_out = binary_forward_cuda(
+  at::Tensor d_out = binary_forward::binary_forward_cuda(
     d_input,
     d_weight,
     IN_SIZE / 2);
@@ -463,7 +483,7 @@ int main( int argc, char *argv[] )
     printf("HERE 10\n");
   
     // do convolution on cpu
-    at::Tensor h_out = host_helper(h_input, h_weight, IN_SIZE / 2);
+    at::Tensor h_out = binary_forward::host_helper(h_input, h_weight, IN_SIZE / 2);
     printf("HERE 11\n");
   
     // stop timers
@@ -477,8 +497,6 @@ int main( int argc, char *argv[] )
     checkCUDA( cudaEventDestroy( stop ) );
   
     // compare GPU implementation results with CPU results
-    float temp = 0.0;
-
     int diff = torch::sum(torch::abs(h_out - d_out.to(torch::kCPU))).item<int>();
     
     printf("error is %d\n",diff);
